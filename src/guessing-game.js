b4bea5a147e496f2bcabe1871bbac93deaa59110
NnnastyaA <nastyafedorets@mail.ru>
2022-08-17 01:28:16 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.currentGuess = 0;
+        this.minValue = 0;
+        this.maxValue = 0;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
+        this.currentGuess = Math.round((this.maxValue - this.minValue) / 2 + this.minValue);
 
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.maxValue = this.currentGuess;
     }
 
     greater() {
-
+        this.minValue = this.currentGuess;
     }
 }
 
